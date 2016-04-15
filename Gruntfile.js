cc966ba1812622bd19188662ae46ec5ece7093c0
Maria Ines Parnisari <maineparnisari@gmail.com>
2016-04-14 23:42:41 -0300

    Added tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-testem');
     
     grunt.initConfig({
         jshint: {
@@ -8,10 +9,35 @@ module.exports = function(grunt) {
             options: {
                 globals: {
                     _: false,
-                    $: false
+                    $: false,
+                    jasmine: false,
+                    describe: false,
+                    it: false,
+                    expect: false,
+                    beforeEach: false,
+                    afterEach: false,
+                    sinon: false
                 },
                 browser: true,
                 devel: true
+            }
+        },
+        testem: {
+            unit: {
+                options: {
+                    framework: 'jasmine2',
+                    launch_in_dev: 'PhantomJS',
+                    before_tests: 'grunt jshint',
+                    serve_files: [
+                        'node_modules/sinon/pkg/sinon.js',
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ],
+                    watch_files: [
+                        'src/**/*.js',
+                        'test/**/*.js'
+                    ]
+                }
             }
         }
     });
---
+++ test/helloSpec.js
@@ -0,0 +1,5 @@
+describe('Hello', function() {
+    it('Says hello', function() {
+        expect(sayHello()).toBe('Hello, world');
+    });
+});
